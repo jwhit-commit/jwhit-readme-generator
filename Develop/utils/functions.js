7f5e8d2e8208b5b0704aec430df05a4325915d81
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-10-11 22:44:57 -0500

    Creating basic file/function structure

---
+++ Develop/utils/functions.js
@@ -0,0 +1,10 @@
+
+
+// TODO: Create a function to generate markdown for README
+const genMarkdown = ({appName}) => 
+`# ${appName}`;
+
+module.exports = {
+    genMarkdown,
+};
+
---
+++ Develop/utils/questions.js
@@ -0,0 +1,12 @@
+//Create an array of questions for user input
+const questions = [
+    {
+        type:'input',
+        message: 'What is title of your app',
+        name: 'appName',
+    },
+]
+
+module.exports = {
+    questions,
+};
